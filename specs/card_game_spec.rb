9bfb146895d895543b45ceb102e75a01e361d3c4
hayden <haydenwb@gmail.com>
2019-08-31 18:02:15 +0100

    test shells created

--- specs/card_game_spec.rb
+++ specs/card_game_spec.rb
@@ -2,13 +2,26 @@ require("pry")
 require("minitest/autorun")
 require("minitest")
 require_relative("../card_game")
+require_relative("../card")
 
-class BusStopTest < MiniTest::Test
+class CardGameTest < MiniTest::Test
 
   def setup
+    @card1 = Card.new("Hearts", 8)
+    @card2 = Card.new("Spades", 1)
+    @cardgame = CardGame.new()
+  end
 
+  def test_checkforAce
 
   end
 
+  def test_highest_card
+
+  end
+
+  def test_self_cards_total
+
+  end
 
 end
